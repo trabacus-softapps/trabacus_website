1ea606fda5729af383a1652115e63cbc78e95d47
murali-shankar <murali.shankar@trabacus.com>
2022-03-14 09:33:07 +0530

    website changes

---
+++ additional_addons/trabacus_website/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Trabacus Website',
+    'version': '15.0',
+    'category': 'Website/Website',
+    'sequence': 0,
+    'author' : 'Trabacus',
+    'summary': 'Trabacus Website',
+    'website': 'https://trabacus.com',
+    'description': """Trabacus Website""",
+    'depends': [ 'base', 'product', 'web', 'website'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+}
